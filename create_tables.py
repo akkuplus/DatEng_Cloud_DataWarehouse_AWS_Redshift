be4ffcec9a3006b554e1c62f462feae262ffc48b
akkuplus <github@akkuplus.33mail.com>
2021-07-31 18:58:31 +0200

    - reviewed queries to work with Redshift - added minimal error error-check block to monitor error during SQL queries or Python execution

--- create_tables.py
+++ create_tables.py
@@ -1,4 +1,5 @@
 import configparser
+import pandas as pd
 import psycopg2
 from sql_queries import create_table_queries, drop_table_queries
 
@@ -15,17 +16,52 @@ def create_tables(cur, conn):
         conn.commit()
 
 
-def main():
+def get_connection():
     config = configparser.ConfigParser()
-    config.read('dwh.cfg')
+    config.read_file(open('dwh.cfg'))
 
-    conn = psycopg2.connect(f"host={config['CLUSTER']['HOST']} dbname={config['CLUSTER']['DB_NAME']} "
-                            f"user={config['CLUSTER']['DB_USER']} password={config['CLUSTER']['DB_PASSWORD']} "
-                            f"port={config['CLUSTER']['DB_PORT']}")
+    DWH_DB = config.get("CLUSTER", "DB_NAME")
+    DWH_DB_USER = config.get("CLUSTER", "DB_USER")
+    DWH_DB_PASSWORD = config.get("CLUSTER", "DB_PASSWORD")
+    DWH_PORT = config.get("CLUSTER", "DB_PORT")
+    DWH_HOST = config.get("CLUSTER", "HOST")
+
+    conn_string = f"postgresql://{DWH_DB_USER}:{DWH_DB_PASSWORD}@{DWH_HOST}:{DWH_PORT}/{DWH_DB}"
+    conn = psycopg2.connect(conn_string)
     cur = conn.cursor()
 
-    drop_tables(cur, conn)
-    create_tables(cur, conn)
+    return conn, cur
+
+
+def main():
+    conn, cur = get_connection()
+
+    try:
+
+        drop_tables(cur, conn)
+        create_tables(cur, conn)
+
+        cur.execute("SELECT DISTINCT tablename FROM pg_table_def WHERE schemaname = 'public';")
+        res = cur.fetchall()
+        print(f"Tables in database:\n{res}")
+
+    except Exception as ex:
+        conn.rollback()
+        cur.execute("SELECT * FROM stl_load_errors;")
+        res = cur.fetchall()
+
+        cols = ["userid", "slice", "tbl", "starttime", "session", "query", "filename", "line_number",
+               "colname", "type", "col_length", "position", "raw_line", "raw_field_value", "err_code", "err_reason",
+               "is_partial", "start_offset"]
+
+        df = pd.DataFrame(res, columns=cols)
+
+        print(f"Error in SQL:\n{df}\n")
+        print(f"Error in Python:\n{ex}")
+
+        #cur.execute("SELECT * FROM STL_LOADERROR_DETAIL;")
+        #res = cur.fetchall()
+        #print(f"Error in detail: {res}")
 
     conn.close()
 
