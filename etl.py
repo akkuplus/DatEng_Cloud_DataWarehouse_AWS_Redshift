3eab64a8260fa533c676efa8f31bc2bfba55f389
akkuplus <github@akkuplus.33mail.com>
2021-07-31 20:28:00 +0200

    added some links regarding: 1) S3 access, 2) and in order to debug errors during imports / loads in Redshift

--- etl.py
+++ etl.py
@@ -8,14 +8,14 @@ def load_staging_tables(cur, conn):
     for query in copy_table_queries:
         cur.execute(query)
         conn.commit()
-        print(f"Loading data successful ({query})")
+        print(f"\nLOADING data successful:\n{query}")
 
 
 def insert_tables(cur, conn):
     for query in insert_table_queries:
         cur.execute(query)
         conn.commit()
-        print(f"Inserting successful ({query})")
+        print(f"\nINSERTING successful:\n {query}")
 
 
 def get_connection():
@@ -40,12 +40,13 @@ def main():
 
     try:
         load_staging_tables(cur, conn)
-        # https://stackoverflow.com/questions/38737014/amazon-redshift-copy-always-return-s3serviceexceptionaccess-denied-status-403
         # https://stackoverflow.com/questions/40524700/s3-to-redshift-copy-with-access-denied
         # https://stackoverflow.com/questions/36398734/copying-data-from-s3-to-redshift-access-denied/40523899?noredirect=1#comment68288222_40523899
         # https://stackoverflow.com/questions/49139459/aws-redshift-copy-with-iam-role
 
         insert_tables(cur, conn)
+        # https://medium.com/@abacigil/how-can-i-troubleshoot-a-failed-amazon-redshift-data-load-the-right-fast-way-30b17b4f193e
+        # https://docs.aws.amazon.com/redshift/latest/dg/r_STL_LOAD_ERRORS.html
     except Exception as ex:
         conn.rollback()
         cur.execute("SELECT * FROM stl_load_errors;")
@@ -55,9 +56,11 @@ def main():
                "colname", "type", "col_length", "position", "raw_line", "raw_field_value", "err_code", "err_reason",
                "is_partial", "start_offset"]
 
-        df = pd.DataFrame(res, columns=cols)
+        if res:
+            df = pd.DataFrame(res, columns=cols)
+            print(f"Error in SQL:\n{df}\n")
 
-        print(f"Error in SQL:\n{df}\n")
+
         print(f"Error in Python:\n{ex}")
 
         #cur.execute("SELECT * FROM STL_LOADERROR_DETAIL;")
